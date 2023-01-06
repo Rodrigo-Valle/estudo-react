40f4a274f629f2ed27479d256c8ddd93323ac020
Rodrigo-Valle <rodrigocostavalle@gmail.com>
2023-01-05 23:48:29 -0300

    react router

---
+++ secao6-react-router/react-router/src/pages/Search.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useSearchParams, Link } from 'react-router-dom';
+import { useFetch } from '../hooks/useFetch';
+
+const Search = () => {
+
+  const [searchParams] = useSearchParams();
+
+  const url = "http://localhost:3000/products?" + searchParams;
+
+  const { data: items, loading, error } = useFetch(url);
+
+  return (
+    <div>
+      <h1>resultados:</h1>
+      {error && (<p>{error}</p>)}
+      <ul className="products">
+        {items && items.map((product) => (
+          <li key={product.id} >
+            <h2>{product.name}</h2>
+            <p>R$: {product.price}</p>
+            <Link to={`/products/${product.id}`} >Detalhes</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Search;
